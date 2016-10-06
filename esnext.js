15f5b07145477eef0277bbe6ddfbbd7db7a1184b
Denis Malinochkin <dmalinochkin@rambler.ru>
2016-10-06 23:36:22 +0300

    Use tabs & update to current XO rules

--- esnext.js
+++ esnext.js
@@ -5,18 +5,20 @@ const path = require('path');
 const baseRules = require('./index');
 
 module.exports = {
-  rulesDirectory: [
-    path.join(path.dirname(require.resolve('tslint-eslint-rules')), 'dist/rules'),
-    path.dirname(require.resolve('tslint-microsoft-contrib'))
-  ],
-  rules: Object.assign({
-    // Not implement:
-    //   * prefer-arrow-callback
-    //   * generator-star-spacing
-    //   * object-shorthand
-    //   * no-await-in-loop
+	rulesDirectory: [
+		path.join(path.dirname(require.resolve('tslint-eslint-rules')), 'dist/rules'),
+		path.dirname(require.resolve('tslint-microsoft-contrib'))
+	],
+	rules: Object.assign({
+		// Not implement:
+		//   * prefer-arrow-callback
+		//   * generator-star-spacing
+		//   * object-shorthand
+		//   * no-await-in-loop
+		//   * prefer-template
+		//   * prefer-numeric-literals
 
-    'no-var-keyword': true, // no-var
-    'prefer-const': true // No option `destructuring: 'all'`
-  }, baseRules.rules)
+		'no-var-keyword': true, // no-var
+		'prefer-const': true // No option `destructuring: 'all'`
+	}, baseRules.rules)
 };
