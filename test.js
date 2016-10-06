e00eb0cb8a8fe4e8af7328e254197fc1ecdbe97f
Denis Malinochkin <dmalinochkin@rambler.ru>
2016-10-06 23:35:59 +0300

    Use tabs for tests

--- test.js
+++ test.js
@@ -8,29 +8,29 @@ const nextRules = require('./esnext');
 const baseRules = require('./index');
 
 describe('Integration', () => {
-  it('Should apply the BASE rules.', () => {
-    const content = fs.readFileSync('./fixtures/base.ts', 'utf8');
-    const tsLint = new Linter('base.ts', content, {
-      formatter: 'json',
-      configuration: baseRules
-    });
+	it('Should apply the BASE rules.', () => {
+		const content = fs.readFileSync('./fixtures/base.ts', 'utf8');
+		const tsLint = new Linter('base.ts', content, {
+			formatter: 'json',
+			configuration: baseRules
+		});
 
-    const result = tsLint.lint();
+		const result = tsLint.lint();
 
-    assert.equal(result.failures[0].ruleName, 'no-constant-condition');
-    assert.equal(result.failures[1].ruleName, 'indent');
-  });
+		assert.equal(result.failures[0].ruleName, 'no-constant-condition');
+		assert.equal(result.failures[1].ruleName, 'indent');
+	});
 
-  it('Should apply the ESNEXT rules.', () => {
-    const content = fs.readFileSync('./fixtures/esnext.ts', 'utf8');
-    const tsLint = new Linter('esnext.ts', content, {
-      formatter: 'json',
-      configuration: nextRules
-    });
+	it('Should apply the ESNEXT rules.', () => {
+		const content = fs.readFileSync('./fixtures/esnext.ts', 'utf8');
+		const tsLint = new Linter('esnext.ts', content, {
+			formatter: 'json',
+			configuration: nextRules
+		});
 
-    const result = tsLint.lint();
+		const result = tsLint.lint();
 
-    assert.equal(result.failures[0].ruleName, 'no-var-keyword');
-    assert.equal(result.failures[1].ruleName, 'prefer-const');
-  });
+		assert.equal(result.failures[0].ruleName, 'no-var-keyword');
+		assert.equal(result.failures[1].ruleName, 'prefer-const');
+	});
 });
